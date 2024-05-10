d07bcfb88fc84494c4ead23dc3da43ed58571ca3
AG3NT AG3NT <mafaesamafaesa79@gmail.com>
2024-05-10 14:43:44 +0200

    First Change

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Agent");
+        String name = input.next();
+        System.out.println("Welcome"+ name);
     }
 }
